ccf1d0715b7997761e793ea1c9179e50961d96d8
ernilson <ernilson25@gmail.com>
2020-09-15 14:34:52 -0300

    primeiro moble app

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+
 
 export default function App() {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    const imc2 = imc;
+    const imc3 = imc;
+    
+    if(imc > 18.6){ alert('Você esta abaixo do peso!' + imc.toFixed(2));
+    }else if(imc2 >= 18.6 && imc2 < 24.9){alert(' peso ideal!' + imc2.toFixed(2));
+    }else if(imc3 >= 24.9 && imc3 < 31.1){
+      alert('Você esta acima do peso!' + imc.toFixed(2));
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={styles.title}>Calcule seu Peso</Text>
+
+     <TextInput 
+     style={styles.input}
+     value={peso}
+     onChangeText={ (peso) => setPeso(peso) }
+     placeholder="Peso (kg)"
+     keyboardType="numeric"
+     />     
+
+<TextInput 
+     style={styles.input}
+     value={altura}
+     onChangeText={ (altura) => setAltura(altura) }
+     placeholder="altura (mtrs)"
+     keyboardType="numeric"
+     />    
+
+     <TouchableOpacity style={styles.button}
+     onPress={handleSubmit}
+     >
+       <Text style={styles.buttonText}>Calcule</Text>
+       </TouchableOpacity> 
     </View>
   );
 }
@@ -14,8 +52,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+  title:{
+    textAlign: 'center',
+    marginTop:25,
+    fontSize:30
+  },
+  input:{
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin: 15,
+    padding:15,
+    color: '#FFF',
+    fontSize: 23
+  },
+  button:{
+    justifyContent: 'center',
+    alignItems:'center',
+    margin:15,
+    padding:15,
+    backgroundColor:'#41Aef4',
+    fontSize:20
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 25,
+  }
 });
